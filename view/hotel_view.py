36b733dee9cc95072cdb6af277f5d2557ba0b5f8
Douglas Silva <douglasitpro@gmail.com>
2024-06-20 07:57:54 -0300

    Arquivo de view para interação com usuário

---
+++ view/hotel_view.py
@@ -0,0 +1,28 @@
+class HotelView:
+    @staticmethod
+    def exibir_menu():
+        print("1. Adicionar Cliente")
+        print("2. Adicionar Quarto")
+        print("3. Fazer Reserva")
+        print("4. Cancelar Reserva")
+        print("5. Listar Reservas")
+        print("0. Sair")
+    
+    @staticmethod
+    def obter_detalhes_quarto():
+        numero = int(input("Número do quarto: "))
+        tipo = input("Tipo do quarto: ")
+        preco = float(input("Preço do quarto: "))
+        return numero, tipo, preco
+    
+    @staticmethod
+    def obter_detalhes_reserva():
+        cpf_cliente = input("CPF do cliente: ")
+        numero_quarto = int(input("Número do quarto: "))
+        data_checkin = input("Data de check-in (YYYY-MM-DD): ")
+        data_checkout = input("Data de check-out (YYYY-MM-DD): ")
+        return cpf_cliente, numero_quarto, data_checkin, data_checkout
+    
+    @staticmethod
+    def exibir_mensagem(mensagem):
+        print(mensagem)
