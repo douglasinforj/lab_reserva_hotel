85dccb022e418319567d25ee6fa1a947bede8195
Douglas Silva <douglasitpro@gmail.com>
2024-06-19 22:38:09 -0300

    Criando script para criação das tabelas do projeto (Clientes, Quartos, Reservas)

--- model/hotel.py
+++ model/hotel.py
@@ -23,8 +23,40 @@ class Hotel:
                 print("Conexão com o banco de dados MYSQL foi bem Sucedida!")
                 return conexao
         except Error as e:
-            print(f"Erro ao conetar ao MYSQL: {e}")
+            print(f"Erro ao conectar ao MYSQL: {e}")
             return None
 
+        
+    
+    def criar_tabelas(self):
+        cursor = self.conexao.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Clientes (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       nome VARCHAR(255) NOT NULL,
+                       cpf VARCHAR(14) NOT NULL
+                       )
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Quartos (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       numero INT NOT NULL,
+                       tipo VARCHAR(50) NOT NULL,
+                       preco DECIMAL(10, 2) NOT NULL
+                       )
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Reservas (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       cliente_id INT,
+                       quarto_id INT,
+                       data_checkin DATE NOT NULL,
+                       data_checkout DATE NOT NULL,
+                       FOREIGN KEY (cliente_id) REFERENCES Clientes(id),
+                       FOREIGN KEY (quarto_id) REFERENCES Quartos(id)
+                       )
+        """)
+        self.conexao.commit()
 
-
+# instanciando a Class para teste
+hotel = Hotel("Hotel Exemplo")
