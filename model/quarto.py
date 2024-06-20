8071d7be75ef1846380f1e309c8209b3753fa5a4
Douglas Silva <douglasitpro@gmail.com>
2024-06-19 23:47:41 -0300

    Criando a classe Quarto para instancia em controller.py

---
+++ model/quarto.py
@@ -0,0 +1,8 @@
+#criando class que será instanciadas no controller.py
+
+class Quarto:
+    def __init__(self, numero, tipo, preco):
+        self.numero = numero
+        self.tipo = tipo
+        self.preco = preco
+        
